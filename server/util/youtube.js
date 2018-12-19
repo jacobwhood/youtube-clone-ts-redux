1b4bd36e0bf17cf5b177b593e33ca95ce972f9a5
jacobwhood <jacobwhood@utexas.edu>
2018-12-19 17:36:29 -0600

    Create utility file to handle communication with YouTube

---
+++ server/util/youtube.js
@@ -0,0 +1,27 @@
+const youtubeApi = require('googleapis').google.youtube('v3');
+
+/**
+ * Search Youtube API for related videos to input query.
+ * @param {String} query Search query to pass to API.
+ * @param {Function} cb Callback function to execute upon response from API.
+ * @returns {Array} Array of up to 10 objects representing related videos. 
+ */
+module.exports.searchVideos = (query, cb) => {
+  console.log('query server-side method: ', query);
+  const options = {
+    key: process.env.YOUTUBE_API_KEY,
+    maxResults: 10,
+    part: 'snippet',
+    q: query,
+    type: 'video'
+  };
+
+  youtubeApi.search.list(options, (err, response) => {
+    if (err) {
+      console.log('error retreiving search results from Youtube APi: ', err);
+      cb(err);
+    } else {
+      cb(null, response.data);
+    }
+  });
+};
