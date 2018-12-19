c9ea7c3811e27f920834e74dfdd5f9c43f5658a6
jacobwhood <jacobwhood@utexas.edu>
2018-12-19 12:52:05 -0600

    Render main components for app

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,9 +1,15 @@
 import * as React from 'react';
+import { Nav } from './Nav';
+import { VideoPlayer } from './VideoPlayer';
+import { VideoList } from './VideoList';
 
 export const App: React.FunctionComponent = () => (
   <div>
     <h2>
       Hello world!
     </h2>
+    <Nav />
+    <VideoPlayer />
+    <VideoList />
   </div>
 );
