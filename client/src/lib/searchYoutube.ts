1a64c6434bb9a635fe323e3a1c2c94d3976c58a6
jacobwhood <jacobwhood@utexas.edu>
2018-12-18 18:04:33 -0600

    Initial project setup

---
+++ client/src/lib/searchYoutube.ts
@@ -0,0 +1,13 @@
+interface IOptionsObject {
+
+};
+
+interface ISearchResult {
+
+};
+
+const searchYoutube = (options: IOptionsObject): ISearchResult[] => {
+  return new Array() as ISearchResult[];
+};
+
+export default searchYoutube;
