ef3bbb9c2712d8c04df4a1ac8e464af2f1f0704b
jacobwhood <jacobwhood@utexas.edu>
2018-12-19 11:32:12 -0600

    Add import statement for styles

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 import { App } from './components/App';
+import './static/styles.css';
 
 render(<App />, document.getElementById('root'));
