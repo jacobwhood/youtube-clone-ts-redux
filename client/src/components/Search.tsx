2774b9c7f34a697d5e8aea168b4410f3dbf5f016
jacobwhood <jacobwhood@utexas.edu>
2018-12-19 17:34:49 -0600

    Add button to kick off YouTube API search

--- client/src/components/Search.tsx
+++ client/src/components/Search.tsx
@@ -1,7 +1,10 @@
 import * as React from 'react';
+import { searchYouTube } from '../api/searchYoutube';
 
 export const Search: React.FunctionComponent = () => (
   <div className='search-container'>
-  
+    <button onClick={() => searchYouTube('arsenal fc')}>
+      Click me!!
+    </button>
   </div>
 );
