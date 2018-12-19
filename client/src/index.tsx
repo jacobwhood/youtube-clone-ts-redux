313f31047db4e2a1a62397b0f5ffbd3c6265de9d
jacobwhood <jacobwhood@utexas.edu>
2018-12-19 12:15:33 -0600

    Update path to styles sheet

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 import { App } from './components/App';
-import './static/styles.css';
+import './styles/styles.css';
 
 render(<App />, document.getElementById('root'));
