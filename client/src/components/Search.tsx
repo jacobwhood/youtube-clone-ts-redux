1f989bf3786f41f8eeb8ad1a46655aca93ee6ddf
jacobwhood <jacobwhood@utexas.edu>
2018-12-23 11:22:22 -0600

    Add ref to capture user input

--- client/src/components/Search.tsx
+++ client/src/components/Search.tsx
@@ -1,10 +1,23 @@
 import * as React from 'react';
 import { searchYouTube } from '../api/searchYoutube';
 
-export const Search: React.FunctionComponent = () => (
-  <div className='search-container'>
-    <button onClick={() => searchYouTube('arsenal fc')}>
-      Click me!!
-    </button>
-  </div>
-);
+
+export const Search: React.SFC = () => {
+  const searchInput = React.createRef<HTMLInputElement>();
+
+  function handleSearch() {
+    if (searchInput.current) {
+      searchYouTube(searchInput.current.value);
+    }
+  };
+
+  return (
+    <div className='search-container'>
+      <input type='text' ref={searchInput} />
+      
+      <button onClick={handleSearch}>
+        Click me!!
+      </button>
+    </div>
+  );
+};
