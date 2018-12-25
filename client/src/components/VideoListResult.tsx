bc90c5a78f84f5c38a5756a3090c9b36f3d1316c
jacobwhood <jacobwhood@utexas.edu>
2018-12-25 13:12:48 -0600

    Add html to display appropriate info for each VideoListResult

--- client/src/components/VideoListResult.tsx
+++ client/src/components/VideoListResult.tsx
@@ -1,8 +1,12 @@
 import * as React from 'react';
 
 
-export const VideoListResult: React.FunctionComponent = () => (
+export const VideoListResult: React.FunctionComponent = (props) => (
   <div className='video-list-result-container'>
-    
+    <img src='https://i.ytimg.com/vi/608Qz9Sd0UU/default.jpg'/>
+    <div className='video-list-result-text'>
+      <h4>Video Title</h4>
+      <p>Video description</p>
+    </div>
   </div>
 );
