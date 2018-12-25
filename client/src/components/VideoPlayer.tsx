52e78a6d1356005831886b9fb9f2659280cd663d
jacobwhood <jacobwhood@utexas.edu>
2018-12-25 13:13:14 -0600

    Add HTML to display currently playing video

--- client/src/components/VideoPlayer.tsx
+++ client/src/components/VideoPlayer.tsx
@@ -1,8 +1,14 @@
 import * as React from 'react';
 
 
-export const VideoPlayer: React.FunctionComponent = () => (
+const VideoPlayer: React.FunctionComponent = (props) => (
   <div className='video-player-container'>
-
+    <iframe src='https://www.youtube.com/embed/HoXatYjoos8'></iframe>
+    <div>
+      <h3>Video Title</h3>
+      <p>Video description</p>
+    </div>
   </div>
 );
+
+export default VideoPlayer;
