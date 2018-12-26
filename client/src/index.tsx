9c973c869484d3abf8a7966a6689ecd05ed5f1b3
jacobwhood <jacobwhood@utexas.edu>
2018-12-25 21:48:30 -0600

    Render App in Provider container for Redux use

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,13 @@
 import * as React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 import { App } from './components/App';
 import './styles/styles.css';
 
-render(<App />, document.getElementById('root'));
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
