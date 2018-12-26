36cfa245d0390577e515d70799d50a8a3a0c1d96
jacobwhood <jacobwhood@utexas.edu>
2018-12-25 21:44:20 -0600

    Create dummy data array

---
+++ default-data/data.ts
@@ -0,0 +1,34 @@
+import { VideoResult } from '../client/src/types/types';
+
+export const defaultData: VideoResult[] = [
+  {
+    description: 'For more match action, highlights and training videos, make sure you become a digital member and sign up to Arsenal Player. It\'s FREE and is the Home of the ...',
+    'thumbnail_url': 'https://i.ytimg.com/vi/608Qz9Sd0UU/default.jpg',
+    title: 'Flicks and tricks | Arsenal\'s top 5 skills of 2018',
+    videoId: '608Qz9Sd0UU'
+  },
+  {
+    description: 'Two goals from Pierre-Emerick Aubameyang and a late Iwobi strike were enough to see off Burnley and get the Gunners back to winning ways. Mesut Ozil - back ...',
+    'thumbnail_url': 'https://i.ytimg.com/vi/gUBSdVh4uqI/default.jpg',
+    title: 'Ozil is back and Aubameyang\'s on fire! | Arsenal 3 - 1 Burnley | Goals and highlights',
+    videoId: 'gUBSdVh4uqI'
+  },
+  {
+    description: 'We take a look back at 5 of the best saves from Leno, Cech and Ospina from the last year. For more match action, highlights and training videos, make sure you ...',
+    'thumbnail_url': 'https://i.ytimg.com/vi/SWQhBsLf4sU/default.jpg',
+    title: 'Which goalkeeper made the best save for Arsenal in 2018?',
+    videoId: 'SWQhBsLf4sU'
+  },
+  {
+    description: 'After Mesut saw a video of a family naming their child after him on social media, he wanted to send a video message and a gift to thank the family for their ...',
+    'thumbnail_url': 'https://i.ytimg.com/vi/tIkY8qY0pDk/default.jpg',
+    title: 'Mesut Ozil sends Mehd Ozil a gift',
+    videoId: 'tIkY8qY0pDk'
+  },
+  {
+    description: 'Pierre-Emerick Aubameyang scored two goals to lead Arsenal to a 3-1 victory over Burnley. #NBCSports #PremierLeague #Arsenal #Burnley » Subscribe to ...',
+    'thumbnail_url': 'https://i.ytimg.com/vi/rSGMpCb1zRI/default.jpg',
+    title: 'Arsenal v. Burnley | PREMIER LEAGUE EXTENDED HIGHLIGHTS | 12/22/18 | NBC Sports',
+    videoId: 'rSGMpCb1zRI'
+  }
+];
