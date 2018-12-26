641de410bb841c877acbf90551c047178e57184a
jacobwhood <jacobwhood@utexas.edu>
2018-12-25 21:49:59 -0600

    Complete and rename main reducers file

---
+++ client/src/reducers/reducers.ts
@@ -0,0 +1,30 @@
+import { UPDATE_VIDEO_LIST, UPDATE_CURRENT_VIDEO } from '../actions/actionTypes';
+import { VideoResult, Action, State } from '../types/types';
+import { defaultData } from '../../../default-data/data';
+
+let defaultCurrentVideo: VideoResult = defaultData[0];
+let defaultVideoList: VideoResult[] = defaultData;
+
+let defaultState = {
+  currentVideo: defaultCurrentVideo,
+  videos: defaultVideoList
+};
+
+const reducers = (state: State = defaultState, action: Action): State => {
+  switch(action.type) {
+    case UPDATE_VIDEO_LIST:
+      return {
+        ...state,
+        videos: action.payload
+      }
+    case UPDATE_CURRENT_VIDEO:
+      return {
+        ...state,
+        currentVideo: action.payload
+      }
+    default:
+      return state
+  }
+};
+
+export default reducers;
