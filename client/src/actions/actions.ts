e379dd6056d5ed46a93f8c8d5c91b0d62294073d
jacobwhood <jacobwhood@utexas.edu>
2018-12-31 10:50:35 -0600

    Create new thunk actions for async work

--- client/src/actions/actions.ts
+++ client/src/actions/actions.ts
@@ -1,7 +1,11 @@
-import { VideoResult, Action } from '../types/types';
-import { UPDATE_VIDEO_LIST, UPDATE_CURRENT_VIDEO } from './actionTypes';
+import { Dispatch, ActionCreator, AnyAction } from 'redux';
 
-export const updateVideoList = (videos: VideoResult[]): Action => ({
+import { VideoResult, Action} from '../types/types';
+import { UPDATE_VIDEO_LIST, UPDATE_CURRENT_VIDEO } from './actionTypes';
+import { searchYouTube } from '../api/searchYoutube';
+
+
+export const updateVideoList: ActionCreator<Action> = (videos: VideoResult[]): Action => ({
     type: UPDATE_VIDEO_LIST,
     payload: videos
 });
@@ -10,3 +14,24 @@ export const updateCurrentVideo = (video: VideoResult): Action => ({
     type: UPDATE_CURRENT_VIDEO,
     payload: video
 });
+
+export const updateCurrentVideoThunkAction = (video: VideoResult) => {
+    return (dispatch: Dispatch<AnyAction>) => {
+        dispatch(updateCurrentVideo(video));
+    };
+};
+
+
+export const searchYouTubeAsync = (q: string) => {
+    return async (dispatch: Dispatch<Action>): Promise<Action> => {
+        try {
+            let videos = await searchYouTube(q);
+
+            videos ? dispatch(updateCurrentVideo(videos[0] as VideoResult)) : null;
+            return dispatch(updateVideoList(videos as VideoResult[]));
+        }
+        catch(err) {
+            throw new Error(`Error retrieving videos from YouTube API: ${err}`);
+        }
+    };
+};
