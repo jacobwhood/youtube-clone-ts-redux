a284c72bd51a3cad4f2bfa1eb46aff0ffbe21d15
jacobwhood <jacobwhood@utexas.edu>
2018-12-31 10:55:22 -0600

    Add mapDispatchToProps function

--- client/src/components/VideoPlayer.tsx
+++ client/src/components/VideoPlayer.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
+import { Dispatch } from 'redux';
 
 import { VideoResult, State } from '../types/types';
 
 interface IVideoPlayerProps {
   video: VideoResult;
 }
+
 
 const VideoPlayer: React.FunctionComponent<IVideoPlayerProps> = ({ video }) => (
     <div className='video-player-container'>
@@ -21,4 +23,6 @@ const mapStateToProps = (state: State) => ({
   video: state.currentVideo
 });
 
-export default connect(mapStateToProps, null)(VideoPlayer);
+const mapDispatchToProps = (dispatch: Dispatch) => ({ });
+
+export default connect(mapStateToProps, mapDispatchToProps)(VideoPlayer);
