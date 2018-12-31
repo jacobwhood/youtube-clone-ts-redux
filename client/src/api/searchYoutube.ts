c44c9462e307af6b90b4e905c18a7709eadd8e0e
jacobwhood <jacobwhood@utexas.edu>
2018-12-31 10:55:56 -0600

    Update return type, use async/await, remove shaper helper to separate file

--- client/src/api/searchYoutube.ts
+++ client/src/api/searchYoutube.ts
@@ -2,33 +2,21 @@
 import axios, { AxiosRequestConfig } from 'axios';
 
 import { VideoResult } from '../types/types';
+import { buildYoutubeSearchResultArray } from '../lib/index';
 
 /**
  * Search YouTube API for videos related to q.
  * Will update Redux store with resulting list of videos.
  * @param q search term for YouTube API call
  */
-export const searchYouTube = (q: string) => {
+export const searchYouTube = async (q: string): Promise<VideoResult[] | void> => {
   let options: AxiosRequestConfig = {
     params: { query: q }
   };
 
-  axios.get('api/videos/search', options)
+  return await axios.get('api/videos/search', options)
     .then( ({ data }) => {
-      console.log(buildYoutubeSearchResultArray(data.items));
+      return buildYoutubeSearchResultArray(data.items);
     })
     .catch(console.log);
-};
-
-function buildYoutubeSearchResultArray(rawJSONObjects: any[]): VideoResult[] {
-  let results: VideoResult[] = rawJSONObjects.map(searchResult => {
-    return {
-      title: searchResult.snippet.title,
-      description: searchResult.snippet.description,
-      thumbnail_url: searchResult.snippet.thumbnails.default.url,
-      videoId: searchResult.id.videoId
-    };
-  });
-
-  return results;
 };
