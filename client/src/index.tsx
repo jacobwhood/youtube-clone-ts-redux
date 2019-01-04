a58c2066311ea4db3da2151b9ea092847966a828
jacobwhood <jacobwhood@utexas.edu>
2019-01-04 13:46:04 -0600

    Update styling

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+
 import { store } from './store/store';
 import { App } from './components/App';
 import './styles/styles.css';
