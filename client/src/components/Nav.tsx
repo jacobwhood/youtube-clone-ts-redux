ad922bdd4e9df29e7aa8d0f00fff3323518108fe
jacobwhood <jacobwhood@utexas.edu>
2019-01-07 10:15:45 -0800

    Remove unneeded container div

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -4,11 +4,9 @@ import Search from './Search';
 
 
 export const Nav: React.FunctionComponent = () => (
-  <div className='container'>
-    <div className='nav-container navbar fixed-top navbar-expand-xl navbar-dark bg-dark'>
-      <div className='navbar-collapse'>
-        <Search />
-      </div>
+  <div className='nav-container navbar fixed-top navbar-expand-xl navbar-dark bg-dark'>
+    <div className='navbar-collapse'>
+      <Search />
     </div>
   </div>
 );
